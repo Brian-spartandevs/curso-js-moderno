8a28e863075a97f0789cd8a9d4645a859046579a
Brian Arrieta <barrieta950@gmail.com>
2025-03-15 09:49:23 -0400

    added string methods

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+const product = 'Monitor 20 pulgadas';
+
+console.log(product); 
+
+// Conocer la cantidad de letras de la cadena de texto
+console.log(product.length); //
+
+
+// Encontrar la posición de un texto
+// console.log(product.indexOf('pulgadas'));
+
+// Conocer si una cadena de texto incluye un texto
+console.log(product.includes('pulgadas')); // true
