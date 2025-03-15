ffd4721eebc276e58e5da7ec2847c888f5f0c49a
Brian Arrieta <barrieta950@gmail.com>
2025-03-14 20:13:42 -0400

    added variables let and const

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,17 @@
+let producto = 'Tablet';
+
+// Reasignar el valor
+
+producto = 'Monitor';
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto); // 20
+
+let precio;
+
+precio = 300;
+
+console.log(precio); // 300
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,15 @@
+const producto = 'Tablet';
+
+// Las constantes no se pueden reasignar
+
+// producto = 'Monitor';
+
+console.log(producto);
+
+// Las constantes deben tener un valor
+
+// const precio;
+// console.log(precio); // Uncaught SyntaxError: Missing initializer in const declaration
+
+const precio = 20;
+console.log(precio); // 20
