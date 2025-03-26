22e62d0183897adc020faebf8638d641a67b512e
Brian Arrieta <barrieta950@gmail.com>
2025-03-26 11:33:34 -0400

    added increments and decrements

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,14 @@
+// Veamos como ir incrementando un valor de 1 en 1 o en determinada cantidad
+
+// Incluir incremento por 1 y menos 1
+
+let puntaje = 5;
+puntaje++; // Incrementa en 1 después de mostrar
+puntaje--; // Decrementa en 1 después de mostrar
+++puntaje; // Incrementa en 1 antes de mostrar
+--puntaje; // Decrementa en 1 antes de mostrar
+
+puntaje += 3; // Incrementa en 3
+puntaje -= 3; // Decrementa en 3
+
+console.log(puntaje);
